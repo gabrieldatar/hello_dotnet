440b6f04dd8b65452da2903f60007b718927b44b
Gabriel Rodrigues <gabriel.quirino@take.net>
2021-09-25 19:17:36 -0300

    Primeiro arquivo dotnet

---
+++ 0_primeiro_programa_dotnet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _0_primeiro_programa_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numVezes=5;
+
+            for(int i=0;i<numVezes;i++){
+                Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+            }
+            
+        }
+    }
+}
