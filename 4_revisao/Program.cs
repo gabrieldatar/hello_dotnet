57a52c609b1464bb37608aa3fb66aae48d5a4ab7
Gabriel Rodrigues <gabriel.quirino@take.net>
2021-09-26 21:21:32 -0300

    revisando os conteúdos

---
+++ 4_revisao/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _4_revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1- Inserir novo aluno.");
+            Console.WriteLine("2- Listar alunos.");
+            Console.WriteLine("3- Calcular média geral.");
+            Console.WriteLine("X- Sair.");
+            Console.Write("Opção: ");
+            Console.WriteLine();
+
+            string opcaoUsuario=Console.ReadLine();
+            
+            while(opcaoUsuario.ToUpper()!= "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                        // -adicionar aluno
+                        break;
+                    case "2":
+                        // listar aluno
+                        break;
+                    case "3":
+                        // calcular a media geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine("Informe a opção desejada: ");
+                Console.WriteLine("1- Inserir novo aluno.");
+                Console.WriteLine("2- Listar alunos.");
+                Console.WriteLine("3- Calcular média geral.");
+                Console.WriteLine("X- Sair.");
+                Console.Write("Opção: ");
+                Console.WriteLine();
+
+                opcaoUsuario=Console.ReadLine();
+
+            }
+        }
+    }
+}
