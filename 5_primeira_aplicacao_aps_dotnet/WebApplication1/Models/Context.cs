65dda93c482e5372adf5807e0fdf7671440ca961
Gabriel Rodrigues <gabriel.quirino@take.net>
2021-09-28 18:25:41 -0300

    Colocando as pastas completas

--- 5_primeira_aplicacao_aps_dotnet/WebApplication1/Models/Context.cs
+++ 5_primeira_aplicacao_aps_dotnet/WebApplication1/Models/Context.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
-    public class Context
+    public class Context:DbContext
     {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;DataBase=WebApplication1;Integrated Security=True");
+        }
     }
 }
--- 5_primeira_aplicacao_aps_dotnet/WebApplication1/Startup.cs
+++ 5_primeira_aplicacao_aps_dotnet/WebApplication1/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 namespace WebApplication1
 {
@@ -24,6 +25,7 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Context>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
